a72582e467ca407e8e5a43084d9d6cfce848d632
Pushpendra Aneshwari <aneshwaripushpendra@gmail.com>
2022-06-01 11:01:13 +0530

    styled nav component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
-import Navs from "./components/Navs";
+// import Navs from "./components/Navs";
 import Show from "./pages/Show";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  mainColors: {
+    blue: "#2400ff",
+    gray: "#c6c6c6",
+    dark: "#353535",
+  },
+};
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/starred" element={<Starred />}></Route>
         <Route exact path="/show/:id" element={<Show />}></Route>
       </Routes>
-    </div>
+    </ThemeProvider>
   );
 }
 
